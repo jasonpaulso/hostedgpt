dbc546f3da72e7408a465912151a0b9c7cdbb6fb
Jason Schulz <jason@schulz.studio>
2024-04-18 13:57:10 -0400

    * feat: add serviceworker.js for offline caching * chore: add serviceworker-rails gem to Gemfile * chore: update manifest.json with new icons and start_url * chore: update head.html.erb with meta tags and link to manifest.json

--- config/routes.rb
+++ config/routes.rb
@@ -31,4 +31,6 @@ Rails.application.routes.draw do
 
   get "/rails/active_storage/postgresql/:encoded_key/*filename" => "active_storage/postgresql#show", as: :rails_postgresql_service
   put "/rails/active_storage/postgresql/:encoded_token" => "active_storage/postgresql#update", as: :update_rails_postgresql_service
+
+
 end
