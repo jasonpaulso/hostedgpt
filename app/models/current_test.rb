39579d375e41cc4a7e8ae3d19cdc0c03ee008a9f
Jason Schulz <jason@schulz.studio>
2024-05-02 11:00:11 -0400

    * fix(current_test.rb): update test description to clarify that it checks if the user method does not override existing preferences

--- app/models/current_test.rb
+++ app/models/current_test.rb
@@ -11,7 +11,7 @@ class CurrentTest < ActiveSupport::TestCase
     assert_equal 'system', user.preferences[:dark_mode]
   end
 
-  test "user method does not override existing preferences" do
+  test "user method does not override existing preferences?" do
     user = User.new
     user.preferences = { dark_mode: 'light' }
     current = Current.new(user)
@@ -20,5 +20,4 @@ class CurrentTest < ActiveSupport::TestCase
 
     assert_equal 'light', user.preferences[:dark_mode]
   end
-
 end
