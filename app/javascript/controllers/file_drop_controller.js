605316ca5fcfd4c5803d5428f998d171bc4931e7
Jason Schulz <jason@schulz.studio>
2024-05-09 09:02:42 -0400

    * fix(styles): add file-drop-area styles to application.tailwind.css * fix(file_drop_controller.js): update event listeners to use draggableTarget instead of element * fix(application.html.erb): add data-file-drop-target attribute to transition controller and scrollable controller * fix(_main_column.html.erb): add data-file-drop-target attribute to scrollable controller * feat(_main_column.html.erb): add file-drop-area element with text prompt
    Improve usability of dragging & dropping files #131

--- app/javascript/controllers/file_drop_controller.js
+++ app/javascript/controllers/file_drop_controller.js
@@ -1,39 +1,39 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['overlay'];
+  static targets = ['overlay', 'body', 'draggable'];
   static classes = ['dragging'];
 
   connect() {
-    this.element.addEventListener('drop', this.boundDropped.bind(this));
-    this.element.addEventListener('dragover', this.handleDragOver.bind(this));
-    this.element.addEventListener('dragleave', this.handleDragLeave.bind(this));
-    this.element.addEventListener('drop', this.handleDrop.bind(this));
-    this.element.addEventListener('dragend', this.handleDragEnd.bind(this));
+    this.draggableTarget.addEventListener('drop', this.boundDropped.bind(this));
+    this.draggableTarget.addEventListener('dragover', this.handleDragOver.bind(this));
+    this.draggableTarget.addEventListener('dragleave', this.handleDragLeave.bind(this));
+    this.draggableTarget.addEventListener('drop', this.handleDrop.bind(this));
+    this.draggableTarget.addEventListener('dragend', this.handleDragEnd.bind(this));
   }
 
   handleDragOver(event) {
     event.preventDefault();
-    this.overlayTarget.classList.remove('hidden');
+    this.overlayTarget.classList.add('dragging');
   }
 
   handleDragLeave(event) {
     event.preventDefault();
-    this.overlayTarget.classList.add('hidden');
+    this.overlayTarget.classList.remove('dragging');
   }
 
   handleDrop(event) {
     event.preventDefault();
-    this.overlayTarget.classList.add('hidden');
+    this.overlayTarget.classList.remove('dragging');
   }
 
   handleDragEnd(event) {
     event.preventDefault();
-    this.overlayTarget.classList.add('hidden');
+    this.overlayTarget.classList.remove('dragging');
   }
 
   disconnect() {
-    this.element.removeEventListener('drop', this.boundDropped);
+    this.draggableTarget.removeEventListener('drop', this.boundDropped);
   }
 
   boundDropped = (event) => {
