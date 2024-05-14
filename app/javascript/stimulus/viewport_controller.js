c429ca00a7d62498bbb2df26ea8e00e52a4b7f5e
Jason Schulz <jason@schulz.studio>
2024-05-14 12:41:26 -0400

    wip: refactor

--- app/javascript/stimulus/viewport_controller.js
+++ app/javascript/stimulus/viewport_controller.js
@@ -3,6 +3,8 @@ import { Controller } from '@hotwired/stimulus'
 const UPDATE_INTERVAL_MS = 10
 
 export default class extends Controller {
+  static targets = ['header', 'headerDiv', 'mainSection', 'nav']
+
   connect() {
     this.updateHeight()
     this.startInterval()
@@ -13,10 +15,18 @@ export default class extends Controller {
   }
 
   updateHeight() {
-    this.element.style.height = `${window.innerHeight}px`
+    // const { element: body, headerTarget, navTarget } = this
+    // console.log('updateHeight', window.innerHeight)
+    // const headerElementHeight = headerTarget?.offsetHeight || 0
+    // if (navTarget) {
+    //   const newTop = `${headerElementHeight}px`
+    //   navTarget.style.top = newTop
+    //   body.style.height = `${window.innerHeight}px`
+    //   // body.style.minHeight = 'auto'
+    // }
   }
 
   startInterval() {
-    this.interval = setInterval(() => this.updateHeight(), UPDATE_INTERVAL_MS)
+    // this.interval = setInterval(() => this.updateHeight(), UPDATE_INTERVAL_MS)
   }
 }
